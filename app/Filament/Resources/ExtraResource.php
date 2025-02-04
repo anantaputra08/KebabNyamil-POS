27d15cabfc1a130a2289deda032a672f7a18e986
PeYoT <putraananta08@gmail.com>
2025-02-04 10:48:52 +0700

    add extras crud

---
+++ app/Filament/Resources/ExtraResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExtraResource\Pages;
+use App\Filament\Resources\ExtraResource\RelationManagers;
+use App\Models\Extra;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExtraResource extends Resource
+{
+    protected static ?string $model = Extra::class;
+    public static ?int $navigationSort = 5;
+    protected static ?string $navigationGroup = 'Product Management';
+    protected static ?string $navigationIcon = 'bxs-category-alt';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Rp.'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money('Rp.')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExtras::route('/'),
+            'create' => Pages\CreateExtra::route('/create'),
+            'view' => Pages\ViewExtra::route('/{record}'),
+            'edit' => Pages\EditExtra::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ExtraResource/Pages/CreateExtra.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\ExtraResource\Pages;
+
+use App\Filament\Resources\ExtraResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateExtra extends CreateRecord
+{
+    protected static string $resource = ExtraResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
